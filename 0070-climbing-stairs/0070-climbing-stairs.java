05e5cfa913a3e8c40eac0b083da0f351cf477b54
jiteshbarwal <122426488+jiteshbarwal@users.noreply.github.com>
2024-11-08 22:30:12 +0530

    Time: 0 ms (100.00%), Space: 40.5 MB (29.44%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.java
+++ 0070-climbing-stairs/0070-climbing-stairs.java
@@ -1,6 +1,6 @@
 class Solution {
     public int climbStairs(int n) {
-        if (n <= 1) return 1; // Base case for 0 or 1
+        if (n <= 1) return 1; 
         
         int dp[] = new int[n + 1];
         dp[1] = 1;
