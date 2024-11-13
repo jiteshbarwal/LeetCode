f2a432fb8725542bf623cde74030e35471c8365d
jiteshbarwal <122426488+jiteshbarwal@users.noreply.github.com>
2024-11-13 20:59:47 +0530

    Time: 1 ms (9.32%), Space: 40.5 MB (28.41%) - LeetHub

--- 0062-unique-paths/0062-unique-paths.java
+++ 0062-unique-paths/0062-unique-paths.java
@@ -1,24 +1,29 @@
 class Solution {
     public int uniquePaths(int m, int n) {
-       
-        int[][] dp = new int[m + 1][n + 1];
-        for (int i = 0; i <= m; i++) {
-            Arrays.fill(dp[i], -1);
-        }
+       int dp[][] = new int[m][n];
 
-       
-        return uniquePathsMemo(m, n, dp);
+        for (int[] row : dp)
+            Arrays.fill(row, -1);
+
+        return countWaysUtil(m, n, dp);
     }
 
-    private int uniquePathsMemo(int m, int n, int[][] dp) {
-       
-        if (m == 1 || n == 1) return 1;
-
-        if (dp[m][n] != -1) return dp[m][n];
-
-        
-        dp[m][n] = uniquePathsMemo(m - 1, n, dp) + uniquePathsMemo(m, n - 1, dp);
-
-        return dp[m][n];
+    static int countWaysUtil(int m, int n, int[][] dp) {
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                if (i == 0 && j == 0) {
+                    dp[i][j] = 1;
+                    continue;
+                }
+                int up = 0;
+                int left = 0;
+                if (i > 0)
+                    up = dp[i - 1][j];
+                if (j > 0)
+                    left = dp[i][j - 1];
+                dp[i][j] = up + left;
+            }
+        }
+        return dp[m - 1][n - 1];
     }
 }
